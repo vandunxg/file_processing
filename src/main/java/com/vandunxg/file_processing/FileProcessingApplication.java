ebd89c2b24f358bc054e07dae94a31531c901818
Nguyen Van Dung <nguyenvandung.official@gmail.com>
2026-07-17 10:22:46 +0700

    chore: build base project

---
+++ src/main/java/com/vandunxg/file_processing/FileProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.vandunxg.file_processing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileProcessingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vandunxg/file_processing/FileProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vandunxg.file_processing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
